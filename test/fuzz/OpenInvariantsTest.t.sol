43bd6d5e2eab1a3f3cf7aa5d6e3eb53c834c35c2
tusharr1411 <tusharr1411@gmail.com>
2025-03-06 02:00:17 +0530

    added fuzz test suite

--- test/fuzz/OpenInvariantsTest.t.sol
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -1,58 +1,58 @@
-// //SPDX-License-Identifier:MIT
+//SPDX-License-Identifier:MIT
 
-// pragma solidity 0.8.24;
+pragma solidity 0.8.24;
 
-// //have our invariants aka properties that our system should always hold
+//have our invariants aka properties that our system should always hold
 
-// //Okay now what are our invariants here ?
+//Okay now what are our invariants here ?
 
-// // 1. Total value of DSC minted should be less than the total value of collateral
-// // 2. Getter view functions should never revert // Everngreen invariant
+// 1. Total value of DSC minted should be less than the total value of collateral
+// 2. Getter view functions should never revert // Everngreen invariant
 
-// import {Test, console} from "forge-std/Test.sol";
-// import {StdInvariant} from "forge-std/StdInvariant.sol";
-// import {DeployDSC} from "../../script/DeployDSC.s.sol";
-// import {HelperConfig} from "../../script/HelperConfig.s.sol";
-// import {DSCEngine} from "../../src/DSCEngine.sol";
-// import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 
-// import {ERC20Mock} from "../mocks/ERC20Mock.sol";
-// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// contract OpenInvariantTest is StdInvariant, Test {
-//     // DeployDSC deployer;
-//     DSCEngine engine;
-//     HelperConfig helperConfig;
-//     DecentralizedStableCoin dsc;
+contract OpenInvariantTest is StdInvariant, Test {
+    // DeployDSC deployer;
+    DSCEngine engine;
+    HelperConfig helperConfig;
+    DecentralizedStableCoin dsc;
 
-//     address public ethUsdPriceFeed;
-//     address public btcUsdPriceFeed;
-//     address public weth;
-//     address public wbtc;
-//     uint256 public deployerKey;
+    address public ethUsdPriceFeed;
+    address public btcUsdPriceFeed;
+    address public weth;
+    address public wbtc;
+    uint256 public deployerKey;
 
-//     function setUp() external {
-//         DeployDSC deployer = new DeployDSC();
-//         (dsc, engine, helperConfig) = deployer.run();
-//         (ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc, deployerKey) = helperConfig.activeNetworkConfig();
+    function setUp() external {
+        DeployDSC deployer = new DeployDSC();
+        (dsc, engine, helperConfig) = deployer.run();
+        (ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc, deployerKey) = helperConfig.activeNetworkConfig();
 
-//         console.log(address(engine));
-//         targetContract(address(engine));
-//     }
+        console.log(address(engine));
+        targetContract(address(engine));
+    }
 
-//     function invariant_protocolMustHaveMoreCollateralValueThanTotalDSCSupply_() public view {
-//         // get the value of all the collateral in  protocol
-//         // Compare it to all the debt ( DSC)
+    function invariant_protocolMustHaveMoreCollateralValueThanTotalDSCSupply_() public view {
+        // get the value of all the collateral in  protocol
+        // Compare it to all the debt ( DSC)
 
-//         uint256 totalDSCSupply = dsc.totalSupply();
-//         uint256 ethCollateralInProtocol = ERC20Mock(weth).balanceOf(address(engine));
-//         uint256 btcCollateralInProtocol = ERC20Mock(wbtc).balanceOf(address(engine));
+        uint256 totalDSCSupply = dsc.totalSupply();
+        uint256 ethCollateralInProtocol = ERC20Mock(weth).balanceOf(address(engine));
+        uint256 btcCollateralInProtocol = ERC20Mock(wbtc).balanceOf(address(engine));
 
-//         uint256 ethCollateralUSDValue = engine.getUsdValue(weth, ethCollateralInProtocol);
-//         uint256 btcCollateralUSDValue =  engine.getUsdValue(wbtc, btcCollateralInProtocol);
-//         uint256 totalUSDValueOfCollaterals = ethCollateralUSDValue + btcCollateralUSDValue;
+        uint256 ethCollateralUSDValue = engine.getUsdValue(weth, ethCollateralInProtocol);
+        uint256 btcCollateralUSDValue =  engine.getUsdValue(wbtc, btcCollateralInProtocol);
+        uint256 totalUSDValueOfCollaterals = ethCollateralUSDValue + btcCollateralUSDValue;
 
-//         assert( totalDSCSupply <= totalUSDValueOfCollaterals);
-//     }
+        assert( totalDSCSupply <= totalUSDValueOfCollaterals);
+    }
 
-// }
+}
